0077d3c1ada3f2f98d026902a8b2c58c74f70b06
Elysia <a.elysia@proton.me>
2026-01-21 12:41:06 +0800

    build: add loongarch64-linux cross compilation support

---
+++ build/loongarch64-linux/crosscompile.cmake
@@ -0,0 +1,19 @@
+# CMake toolchain file for cross compiling x265 for aarch64
+# This feature is only supported as experimental. Use with caution.
+# Please report bugs on bitbucket
+# Run cmake with: cmake -DCMAKE_TOOLCHAIN_FILE=crosscompile.cmake -G "Unix Makefiles" ../../source && ccmake ../../source
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR loongarch64)
+
+# specify the cross compiler (giving precedence to user-supplied CC/CXX)
+if(NOT DEFINED CMAKE_C_COMPILER)
+    set(CMAKE_C_COMPILER loongarch64-linux-gnu-gcc)
+endif()
+if(NOT DEFINED CMAKE_CXX_COMPILER)
+    set(CMAKE_CXX_COMPILER loongarch64-linux-gnu-g++)
+endif()
+
+# specify the target environment
+SET(CMAKE_FIND_ROOT_PATH  /usr/loongarch64-linux-gnu)
+
